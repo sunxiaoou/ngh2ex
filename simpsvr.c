183646f0dfab36d6c25116c4d2fdb507eca9be27
sunxiaoou <sun_xiaoou@qq.com>
2019-10-16 09:40:00 +0800

    add log

--- simpsvr.c
+++ simpsvr.c
@@ -1,48 +1,87 @@
 // Server side C/C++ program to demonstrate Socket programming
+#include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+
+#include "log/log.h"
+
 #define PORT 8080
 
-int main(int argc, char const *argv[]) {
-  int server_fd, new_socket, valread;
-  struct sockaddr_in address;
-  int opt = 1;
-  int addrlen = sizeof(address);
-  char buffer[1024] = {0};
-  char *hello = "Hello from server";
+int listen_to(int port, int nlisten) {
+	PRINT(log_in, "listen_to")
 
-  // Creating socket file descriptor
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-    perror("socket failed");
-    exit(EXIT_FAILURE);
-  }
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket < 0) {
+		perror("socket failed");
+		PRINT(log_out, "listen_to")
+    return -1;
+	}
+	
+	struct sockaddr_in sin;
+	memset(&sin, 0, sizeof(struct sockaddr_in));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	sin.sin_port = htons(port);
 
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
-
-  // Forcefully attaching socket to the port 8080
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+	int rc = bind(sock, (struct sockaddr *)(&sin), sizeof(struct sockaddr_in));
+	if (rc < 0) {
     perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
+		close(sock);
+    PRINT(log_out, "listen_to2")
+		return -1;
+	}
 
-  if (listen(server_fd, 3) < 0) {
-    perror("listen");
-    exit(EXIT_FAILURE);
-  }
+	rc = listen(sock, nlisten);
+	if (rc < 0) {
+    perror("listen failed");
+		close(sock);
+    PRINT(log_out, "listen_to3")
+		return -1;
+	}
 
-  if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+  PRINT(log_out, "listen_to4")
+	return sock;
+}
+
+int accept_to(int listener) {
+  PRINT(log_in, "accept_to")
+
+	struct sockaddr_in sin;
+	unsigned long len = sizeof(struct sockaddr_in);
+	int sock = accept(listener, (struct sockaddr *)(&sin), (socklen_t *)&len);
+	if (sock < 0) {
     perror("accept");
-    exit(EXIT_FAILURE);
+    PRINT(log_out, "accept_to")
+    return -1;
+	}
+
+  PRINT(log_out, "accept_to2")
+	return sock;
+}
+
+int main(int argc, char const *argv[]) {
+  PRINT(log_in, "main")
+
+  int listener = listen_to(PORT, 3);
+  if (listener < 0) {
+    PRINT(log_out, "main")
+    return -1;
   }
 
-  valread = read(new_socket, buffer, 1024);
-  printf("%s\n",buffer);
+  int sock = accept_to(listener);
+  if (sock < 0) {
+    PRINT(log_out, "main2")
+    return -1;
+  }
+
+  char buffer[1024] = {0};
+  int len = read(sock, buffer, 1024);
+  HEXDUMP(buffer, len)
+  fprintf(stderr, "read(%d)\n", len);
 
   char buf1[] = {
     0X48, 0X54, 0X54, 0X50, 0X2F, 0X31, 0X2E, 0X31, 0X20, 0X31, 0X30, 0X31, 0X20, 0X53, 0X77, 0X69,
@@ -61,10 +100,12 @@ int main(int argc, char const *argv[]) {
     0X32, 0X66, 0X0D, 0X0A, 0X0D, 0X0A
   };
 
-  send(new_socket, buf1, sizeof(buf1), 0);
-  printf("Hello message sent\n");
+  HEXDUMP(buf1, sizeof(buf1))
+  len = write(sock, buf1, sizeof(buf1));
+  fprintf(stderr, "written(%d)\n", len);
 
-  close(new_socket);
+  close(sock);
 
+  PRINT(log_out, "main3")
   return 0;
 }
