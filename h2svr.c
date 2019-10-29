20c52a1a9ac576bd6e7b0b73d01a4159f96704b3
sunxiaoou <sun_xiaoou@qq.com>
2019-10-29 10:33:48 +0800

    use respones string

--- h2svr.c
+++ h2svr.c
@@ -518,29 +518,13 @@ int main(int argc, char const *argv[]) {
   // http2_session_data h2session;
   initialize_nghttp2_session(&h2session);
 
-  // return 0;
-
-  char buf1[] = {
-    0X48, 0X54, 0X54, 0X50, 0X2F, 0X31, 0X2E, 0X31, 0X20, 0X31, 0X30, 0X31, 0X20, 0X53, 0X77, 0X69,
-    0X74, 0X63, 0X68, 0X69, 0X6E, 0X67, 0X20, 0X50, 0X72, 0X6F, 0X74, 0X6F, 0X63, 0X6F, 0X6C, 0X73,
-    0X0D, 0X0A, 0X43, 0X6F, 0X6E, 0X6E, 0X65, 0X63, 0X74, 0X69, 0X6F, 0X6E, 0X3A, 0X20, 0X55, 0X70,
-    0X67, 0X72, 0X61, 0X64, 0X65, 0X0D, 0X0A, 0X44, 0X61, 0X74, 0X65, 0X3A, 0X20, 0X54, 0X75, 0X65,
-    0X2C, 0X20, 0X31, 0X35, 0X20, 0X4F, 0X63, 0X74, 0X20, 0X32, 0X30, 0X31, 0X39, 0X20, 0X30, 0X36,
-    0X3A, 0X32, 0X34, 0X3A, 0X30, 0X33, 0X20, 0X47, 0X4D, 0X54, 0X0D, 0X0A, 0X43, 0X6F, 0X6E, 0X74,
-    0X65, 0X6E, 0X74, 0X2D, 0X4C, 0X65, 0X6E, 0X67, 0X74, 0X68, 0X3A, 0X20, 0X30, 0X0D, 0X0A, 0X58,
-    0X2D, 0X4F, 0X52, 0X41, 0X43, 0X4C, 0X45, 0X2D, 0X44, 0X4D, 0X53, 0X2D, 0X52, 0X49, 0X44, 0X3A,
-    0X20, 0X30, 0X0D, 0X0A, 0X55, 0X70, 0X67, 0X72, 0X61, 0X64, 0X65, 0X3A, 0X20, 0X68, 0X32, 0X63,
-    0X0D, 0X0A, 0X58, 0X2D, 0X4F, 0X52, 0X41, 0X43, 0X4C, 0X45, 0X2D, 0X44, 0X4D, 0X53, 0X2D, 0X45,
-    0X43, 0X49, 0X44, 0X3A, 0X20, 0X61, 0X37, 0X33, 0X32, 0X63, 0X38, 0X32, 0X31, 0X2D, 0X61, 0X66,
-    0X32, 0X39, 0X2D, 0X34, 0X33, 0X64, 0X31, 0X2D, 0X38, 0X38, 0X32, 0X30, 0X2D, 0X38, 0X31, 0X34,
-    0X61, 0X32, 0X62, 0X66, 0X38, 0X35, 0X65, 0X64, 0X65, 0X2D, 0X30, 0X30, 0X30, 0X30, 0X30, 0X30,
-    0X32, 0X66, 0X0D, 0X0A, 0X0D, 0X0A
-  };
-
-  HEXDUMP(buf1, sizeof(buf1))
-  len = write(sock, buf1, sizeof(buf1));
+  char respones[] = "HTTP/1.1 101 Switching Protocols\r\n"
+                    "Connection: Upgrade\r\n"
+                    "Upgrade: " NGHTTP2_CLEARTEXT_PROTO_VERSION_ID "\r\n"
+                    "\r\n";
+  HEXDUMP(respones, strlen(respones))
+  len = write(sock, respones, sizeof(respones));
   fprintf(stderr, "written(%d)\n", len);
-
 
   http2_stream_data *stream_data = create_http2_stream_data(&h2session, 1);
   stream_data->request_path = "/";
